d53ceda749b1425a6c6201cb8eee0492b8d32149
Kleberson Duarte Santos <klebersonduartesantos39@gmail.com>
2026-05-27 20:53:15 -0300

    Completo
    Versão funcional entregue na semana da tecnologia da ETEC Lauro Gomes

--- SensorAntiAereo_ETEC.ino
+++ SensorAntiAereo_ETEC.ino
@@ -1,40 +1,91 @@
 #include <Servo.h> // Puxa a biblioteca do Servo Motor
-Servo Garra; 
+Servo Garra; //radar
+Servo servoEixoX; //base
+Servo servoMotor; //lazer
 
+//lógica do radar
+int pos = 0;
+int direcao = 1;
+bool travado = false;
+int anguloAlvo = 0;
 int original =0;
+
+//pinos do radar
+const int TRIG_PIN = 12;
+const int ECHO_PIN = 11;
+
+const int SERVO_PIN = 3;  //servo radar
+const int BASE_PIN = 6;  // servo base
+const int LAZER_PIN = 9; //servo lazer
+
+const int LAZER = 4; //lazer
+
 void setup()
 {
   Serial.begin(9600);
-  pinMode(12, OUTPUT);
-  pinMode(11,INPUT);
-Garra.attach(3);
-  original = lersensor(12,11);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN,INPUT);
+  pinMode(LAZER,OUTPUT);
 
+  Garra.attach(SERVO_PIN);
+  servoEixoX.attach(BASE_PIN);
+  servoMotor.attach(LAZER_PIN);
+
+  original = lersensor(TRIG_PIN,ECHO_PIN);
+  servoEixoX.write(0);
 
 }
 
 void loop()
 {
-  //Gira o motor em 180°
-    for(int i = 0;i <=180;i++){
-     Garra.write(i);
-	delay(30);
-      }
-  
-  //Gira o motor em -180°
-  for(int j= 180; j>=0;j--){
-  Garra.write(j);
-   delay(30);
-    }
-  
-  //Lógica que verifica se tem algo que não tinha antes
-  //não ser se tá funcionando, pois não deu para testar
-  int diferente = lersensor(12,11);
-  if((diferente != original) > 7){ //O 7 é a margem de erro
-  Serial.print("Tem algo novo ai");
+    int diferente = lersensor(TRIG_PIN,ECHO_PIN);
+
+  // Detectou algo
+  if(diferente < 20 && !travado){
+    travado = true;
+    anguloAlvo = pos; // guarda o ândulo
   }
   
+ if(diferente >= 20){
+    travado = false;
+  }
+  // Se não travou, continua girando
+  if(!travado){
+    digitalWrite(LAZER, LOW);
+    servoMotor.write(0);
+    pos += direcao;
+
+    if(pos >= 180 || pos <= 0){
+      direcao *= -1;
+    }
+
+    Garra.write(pos);
+    delay(30);
+  }
+
+  // Se travou, mantém no alvo
+  else{
+    Garra.write(anguloAlvo);
+
+  Serial.println(anguloAlvo);
+//  Move a base pro mesmo ângulo
+    servoEixoX.write(anguloAlvo -30);
+  
+  delay(30);
+  servoMotor.write(75);
+  digitalWrite(LAZER, HIGH);
+
+    
+  }
+
+ 
+  //Lógica que verifica se tem algo que não tinha antes
+
+  if(abs(diferente != original) > 7){ //O 7 é a margem de erro
+  Serial.print("Tem algo novo ai");
+  }
 }
+
 
 //Função do sensor ultrassoônico que detecta os objetos
 int lersensor(int pinotring, int pinoecho){
